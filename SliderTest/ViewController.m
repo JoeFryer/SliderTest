d773120c610a04921560fb26c8f11b603c530bde
Joe Fryer <joe.d.fryer@gmail.com>
2015-10-05 18:49:29 +0100

    Simple slider example.

--- SliderTest/ViewController.m
+++ SliderTest/ViewController.m
@@ -8,20 +8,46 @@
 
 #import "ViewController.h"
 
+
 @interface ViewController ()
+
+@property (nonatomic, weak) IBOutlet UISlider *slider1;
+@property (nonatomic, weak) IBOutlet UISlider *slider2;
+
+@property (nonatomic, weak) IBOutlet UILabel *label1;
+@property (nonatomic, weak) IBOutlet UILabel *label2;
 
 @end
 
+
 @implementation ViewController
 
-- (void)viewDidLoad {
+- (void)viewDidLoad
+{
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.slider1.minimumValue = 0.0f;
+    self.slider1.maximumValue = 100.0f;
+    
+    self.slider2.minimumValue = 0.0f;
+    self.slider2.maximumValue = 100.0f;
+    
+    self.label1.text = @"0";
+    self.label2.text = @"0";
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)slider1ValueChanged:(UISlider *)sender
+{
+    self.label1.text = [NSString stringWithFormat:@"%.0f", sender.value];
+}
+
+- (IBAction)slider2ValueChanged:(UISlider *)sender
+{
+    float max = 100 - self.slider1.value;
+    if (sender.value > max) {
+        sender.value = max;
+    }
+    self.label2.text = [NSString stringWithFormat:@"%.0f", sender.value];
 }
 
 @end
